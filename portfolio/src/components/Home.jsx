b26184775ba927a2af5f1b7ae66e8a81386c9c8c
mu11ern <mu11ern@yahoo.com>
2023-08-05 14:52:43 -0700

    nav bar with socials, no links

---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-[#3e4943] text-[#d4d8c8]'></div>
+  )
+}
+
+export default Home
--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,16 +1,97 @@
- import { FaBars, FaTimes } from 'react-icons/fa'
-import Logo from '../assets/NM-Logo.png'
-
-import React from 'react'
+import React, {useState} from 'react'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+import Logo from "../assets/NM-Logo.png";
 
 const NavBar = () => {
-  return (
-    <div className = 'fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#d4d8c8]'>
-    <div>
-        <img src={Logo} alt="Nicholas Muller Initials Logo" style={{width: '50px'}} />
-    </div>
-    </div>
-  )
-}
+  const [nav, setNav] = useState(false)
 
-export default NavBar
+  const handleClick = () => {
+    setNav(!nav)
+  }
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#d4d8c8] text-[#3e4943]">
+      <div>
+        <img
+          src={Logo}
+          alt="Nicholas Muller Initials Logo"
+          style={{ width: "70px" }}
+        />
+      </div>
+
+      {/* menu */}
+
+        <ul className="hidden md:flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Education</li>
+          <li>Contact</li>
+        </ul>
+
+
+    {/* hamburger */}
+    <div onClick={handleClick} className="md:hidden z-10">
+      {!nav ? <FaBars/> : <FaTimes/>}
+    </div>
+
+
+    {/* mobile */}
+
+    <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#d4d8c8] flex flex-col justify-center items-center'}>
+          <li className="py-6 text-4xl">Home</li>
+          <li className="py-6 text-4xl">About</li>
+          <li className="py-6 text-4xl">Skills</li>
+          <li className="py-6 text-4xl">Projects</li>
+          <li className="py-6 text-4xl">Education</li>
+          <li className="py-6 text-4xl">Contact</li>
+        </ul>
+
+
+    {/* Socials */}
+    <div className='flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+          </ul>
+
+    </div>
+
+    </div>
+  );
+};
+
+export default NavBar;
