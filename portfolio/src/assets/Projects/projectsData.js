5b085b93b8549b8688de27e0b01b8fc786971259
mu11ern <mu11ern@yahoo.com>
2023-08-17 13:19:08 -0700

    projects section. added projects data. need to replace with carousel

---
+++ portfolio/src/assets/Projects/projectsData.js
@@ -0,0 +1,29 @@
+import computational from './computational-imaging.png'
+import placethebet from './placethebet.png'
+import trimet from './trimet-pipeline.png'
+
+export const projectsData=[
+    {
+        id:1,
+        name:"Computational Imaging",
+        image:computational,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:placethebet,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Data Pipeline",
+        image:trimet,
+        github:"",
+        live:"",
+    },
+
+
+]
